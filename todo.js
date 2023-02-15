6cac2344a9c8834e67238082752422d2d819283c
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-15 22:51:00 +0600

    complete , added all todo add button and  single clear butto and all clear button function

--- todo.js
+++ todo.js
@@ -1,12 +1,37 @@
-
-document.getElementById('input-btn').addEventListener('click', function (event) {
-    const inputFiled = document.getElementById('input-value');
-
-    const tr =document.createAttribute('tr');
-    tr.innerHTML+=`
-    <th scope="row"></th>
-    <td>Mark</td>
-    <td>Otto</td>
+let count =0;
+document.getElementById('input-btn').addEventListener('click', function () {
+    count++;
+    const inputFiledPrevious=document.getElementById('input-value')
+    const inputFiled = inputFiledPrevious.value;
+    inputFiledPrevious.value='';
+    const setTodo=document.getElementById('content-container')
+    const tr = document.createElement('tr');
+    
+    tr.innerHTML = `
+    <td>${count}</th>
+    <td>${inputFiled}</td>
+    <td>
+    <button  class="delete-todo btn btn-danger px-4">Delete</button>
+    <button class="complete-todo btn btn-success px-4">Complete</button>
+    </td>
     `
-    console.log(tr);
+    setTodo.appendChild(tr)
+
+
+    const allClass=document.querySelectorAll('.delete-todo');
+    for (const element of allClass) {
+        
+        element.addEventListener('click', function (event) {
+            const removeTr =event.target.parentNode.parentNode.style.display='none';
+            
+        })
+    }
+
+    document.getElementById('clear-btn').addEventListener('click', function () {
+        const clearElements =document.getElementById('content-container');
+
+        clearElements.style.display='none';
+        
+    })
+
 })
